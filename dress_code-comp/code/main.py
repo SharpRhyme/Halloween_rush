6521e3be9107ea4aa55e62a1a5a1a1ec81454397
SharpRhyme <aarizdhalla@hotmail.com>
2023-10-26 19:53:53 +0100

    fixed error where character goes into ground, added animation and background. Tile system was optimised

--- dress_code-comp/code/main.py
+++ dress_code-comp/code/main.py
@@ -1,6 +1,6 @@
 import pygame
-from renderers.utils import render_image, render_tiles
-from renderers.entities import PhysicsEntity
+from renderers.utils import render_image, render_tiles, Animation
+from renderers.entities import PhysicsEntity, Player
 from renderers.tilemap import Tilemap
 import sys
 #imports
@@ -21,25 +21,33 @@ class Game:
         self.assets = {
             "Player": render_image("skelo-static/skelo-warrior.png"),
             "decor": render_tiles("tiles/decor"),
-            "ground": render_tiles("tiles/ground")
+            "ground": render_tiles("tiles/ground"),
+            "bg": render_image("background.png"),
+            "Player/idle": Animation(render_tiles('skelo-idle'), images_dur=7),
+            "Player/jump": Animation(render_tiles("skelo-jump"), images_dur=8)
         }
         
-        self.player = PhysicsEntity(self, 'Player', (50, 50), (8, 15))
+        self.player = Player(self, (50, 50), (32, 30))
 
         self.tilemap = Tilemap(self)
+
+        self.scroll = [0, 0]
         
     
     def run(self):
     #runs game (loops event listener) and updates display while limiting to 60fps
         while True:
-            self.display.fill((142,142,142))
+            self.display.blit(self.assets["bg"], (0, 0))
 
-            self.tilemap.render(self.display)
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.tilemap.render(self.display, offset = render_scroll)
             
             self.player.update(self.tilemap, (self.move[1] - self.move[0], 0))
-            self.player.render(self.display)
-            
-            print(self.tilemap.physics_rects_around(self.player.pos))
+            self.player.render(self.display, offset = render_scroll)
+
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -67,6 +75,13 @@ class Game:
                         self.move[0] = False
                     if event.key == pygame.K_d:
                         self.move[1] = False
+                    
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
+                    if event.key == pygame.K_w:
+                        self.player.velocity[1] = -3
+                    if event.key == pygame.K_SPACE:
+                        self.player.velocity[1] = -3
             
             self.window.blit(pygame.transform.scale(self.display, self.window.get_size()), (0, 0))
             pygame.display.update()
--- dress_code-comp/code/renderers/tilemap.py
+++ dress_code-comp/code/renderers/tilemap.py
@@ -11,7 +11,7 @@ class Tilemap:
         self.offgrid_tiles = []
         
         for i in range(10):
-            self.tilemap[str(3 + i) + ";10"] = {"type": "ground", "variant": 1, "pos": (3 + i, 10)}
+            self.tilemap[str(3 + i) + ";10"] = {"type": "ground", "variant": 1, "pos": (3 + i, 11)}
             #self.tilemap["10;" + str(5 + i)] = {"type": "stone", "variant": 1, "pos": (10, 5 + i)}
             
     def tiles_around(self, pos):
@@ -33,10 +33,17 @@ class Tilemap:
         
         return rects
 
-    def render(self, surf):
+    def render(self, surf, offset=(0, 0)):
         for tile in self.offgrid_tiles:
-            surf.blit(self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] * self.tile_size))
+            surf.blit(self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] - offset[0], tile["pos"][1] - offset[1]))
         
-        for loc in self.tilemap:
-            tile = self.tilemap[loc]
-            surf.blit(self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] * self.tile_size,  tile['pos'][1] * self.tile_size))
+        for x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1):
+            for y in range(offset[1] // self.tile_size, (offset[1] + surf.get_height()) // self.tile_size + 1):
+                loc = f"{str(x)};{str(y)}"
+                if loc in self.tilemap:
+                    tile = self.tilemap[loc]
+                    surf.blit(self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] * self.tile_size - offset[0],  tile['pos'][1] * self.tile_size - offset[1]))
+
+        #for loc in self.tilemap:
+        #    tile = self.tilemap[loc]
+        #    surf.blit(self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] * self.tile_size - offset[0],  tile['pos'][1] * self.tile_size - offset[1]))
